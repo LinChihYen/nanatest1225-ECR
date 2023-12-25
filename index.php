4d9950520efb0e5e13c037a6f78f4b75ea585ea0
NanaCheng63 <143583121+NanaCheng63@users.noreply.github.com>
2023-12-25 15:36:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nana Test :D</title>
+</head>
+<body>	
+	<h1>Nana Test :D Thist is ver5.0</h1>
+	<div id="box1">
+		<form action="http://10.0.1.242:8080/data.php" method="post">
+			<p class="p1">Name :<input type="text" name="name" size="40" id="input0"></p>
+			<p class="p1">Number:<input type="text" name="num" size="40" id="input0"></p>
+		    <input type="submit" name="submit" id="input1">
+	</form>
+	</div>
+	
+
+</body>
+</html>
